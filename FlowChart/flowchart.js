ff05af7322c391fa1e678e6a9db1cd5e46d57371
Madhu Rao <madhu.rao@adelaide.edu.au>
2013-06-05 16:54:57 +0930

    Modified the logic to be generic. logic to enter the text inside the rectangles.

--- FlowChart/flowchart.js
+++ FlowChart/flowchart.js
@@ -1,17 +1,37 @@
 window.onload = function() {
-	var paper = new Raphael(document.getElementById('canvas_container'), 500,
+	var paper = new Raphael(document.getElementById('canvas_container'), 800,
 			500);
-	var tetronimo = paper.rect(200, 200, 160, 80);
 
-	tetronimo.attr({
-		fill : '#9cf',
-		stroke : '#ddd',
-		'stroke-width' : 5,
-		'stroke-linejoin' : 'round'
-	});
+	drawRectangle(paper, 10, 100, 160, 80, [ "Search", "(Begin Here)" ]);
+	drawRectangle(paper, 10, 200, 160, 80, [ "View Single", "Search Results" ]);
+	drawRectangle(paper, 250, 150, 160, 80, [ "Browse Results", "Sumamry" ]);
+	drawRectangle(paper, 500, 150, 160, 80, [ "Download" ]);
 
-	var my_text = paper.text(280, 240, 'my text goes here').attr({
-		fill : '#fff'
-	});
+	function drawRectangle(paper, x, y, width, height, text) {
+		var tetronimo = paper.rect(x, y, width, height);
+
+		tetronimo.attr({
+			fill : '#9cf',
+			stroke : '#ddd',
+			'stroke-width' : 5,
+			'stroke-linejoin' : 'round'
+		});
+
+		var text_x = x + width / 2;
+		var text_y = y + height / 2;
+		for ( var i = 1; i <= text.length; i++) {
+			var font_size = 17 - i;
+			var m_text_y = text_y;
+			if (text.length >= 2) {
+				m_text_y = text_y + (-20 + (20 * (i - 1)));
+			}
+			var my_text = paper.text(text_x, m_text_y, text[i - 1]).attr({
+				fill : '#fff',
+				"font-size" : font_size,
+				"font-family" : "Arial, Helvetica, sans-serif"
+			});
+		}
+
+	}
 
 }
